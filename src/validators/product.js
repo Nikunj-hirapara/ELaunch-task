257c6ad3ad023c0990754ee17d2004fd1515dfee
Nikunj hiarapara <86483981+Nikunj-hirapara@users.noreply.github.com>
2022-02-02 09:27:07 +0530

    Update product.js

--- src/validators/product.js
+++ src/validators/product.js
@@ -4,9 +4,8 @@ exports.validateProduct = [
     check("name").notEmpty().withMessage("product name is required"),
     check("description").notEmpty().withMessage("description is required"),
     check("price").notEmpty().withMessage("price is required"),
-    check("quantity").notEmpty().withMessage("quantity valid email"),
+    check("quantity").notEmpty().withMessage("quantity is required"),
     check("category").notEmpty().withMessage("category is required"),
-    check("createdBy").notEmpty().withMessage("created by is required")
   ];
 
   exports.isRequestValidate = (req, res, next) => {
